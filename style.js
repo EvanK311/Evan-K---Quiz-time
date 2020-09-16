d8acbd4065c14f044aa474e952f09c46550e068a
Evan K <EvanK311@gmail.com>
2020-09-15 20:20:14 -0700

    scrapped bad confusing code for redo

--- style.js
+++ style.js
@@ -1,20 +1,39 @@
+
+//TODO: Create an html that holds quiz format
+//TODO: create a variable that holds array of questions with answers
+//TODO: create function that chooses questions with set of answers
+//TODO: create function that designates what to do with correct/incorrect answers "take 10 seconds off timer"
+//TODO: create function that indicates when game is over.
+//TODO: Create high score page that retains name and high score information.
+
+var startBtn = document.getElementById('startBtn')
+var questionContentBox = document.getElementById('questionContent')
+var randoQuestions, currentQuestionIndex
+
+startBtn.addEventListener('click', starGame) 
+
+function startGame() {
+    startBtn.classList.add('hide')
+    randoQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
+    questionContentBox.classList.remove('hide')
+    nextQuestion()
+}
+
+function nextQuestion() {
+    showQuestion(randoQuestions[CurrentQuestionIndex])
+}
+
+function showQuestion(questions) {
+
+}
+
 var questions = [
     {
-        questText: "Which of these is super effective against Fire",
-         {
-            ansA: 'Water',
-            ansB: 'Fighting',
-            ansC: 'Rock',
-            ansD: 'Flying'
-        },
-    },
-    {
-        questText: "",
-         {
-            ansA: '',
-            ansB: '',
-            ansC: '',
-            ansD: '',
-        },
+        question: "Which if these are fire types weak to",
+        answer: [
+            { text: 'Rock', correct: true },
+            { text: 'Flying', correct: false },
+        ]
     }
-];
+]
